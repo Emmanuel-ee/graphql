58647edf645905a16eea4462e45f316c947aed95
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 14:37:11 +0100

    Field resolvers, adding resolver functions, implement date

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,4 +3,16 @@ export const resolvers = {
   Query: {
     jobs: async () => getJobs(),
   },
+
+  Job: {
+    date: (job) => {
+      //   console.log("resolving date for job", job);
+      //   return "2022-12-31";
+      return toIsoDate(job.createdAt)
+    },
+  },
 };
+
+function toIsoDate(value){
+    return value.slice(0, "yyyy-mm-dd".length)
+}
