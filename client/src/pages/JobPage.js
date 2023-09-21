5d3bcdcf17241e4651f1512592fb62209d965bc7
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 16:40:08 +0100

    Query variables, get single Job

--- client/src/pages/JobPage.js
+++ client/src/pages/JobPage.js
@@ -1,29 +1,33 @@
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import { formatDate } from '../lib/formatters';
-import { jobs } from '../lib/fake-data';
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { formatDate } from "../lib/formatters";
+import { useState, useEffect } from "react";
+import { getJob } from "../lib/qraphql/queries";
+// import { jobs } from "../lib/fake-data";
 
 function JobPage() {
   const { jobId } = useParams();
+  const [job, setJob] = useState();
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
 
-  const job = jobs.find((job) => job.id === jobId);
+  if (!job) {
+    return <div>Loading...</div>;
+  }
+  // const job = jobs.find((job) => job.id === jobId);
+  console.log("[JobPage] job:", job);
   return (
     <div>
-      <h1 className="title is-2">
-        {job.title}
-      </h1>
+      <h1 className="title is-2">{job.title}</h1>
       <h2 className="subtitle is-4">
-        <Link to={`/companies/${job.company.id}`}>
-          {job.company.name}
-        </Link>
+        <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
       </h2>
       <div className="box">
         <div className="block has-text-grey">
-          Posted: {formatDate(job.date, 'long')}
+          Posted: {formatDate(job.date, "long")}
         </div>
-        <p className="block">
-          {job.description}
-        </p>
+        <p className="block">{job.description}</p>
       </div>
     </div>
   );
