5b866aeb26adc918b8dea18a7aa0bcddffa6a52e
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 13:48:29 +0100

    custom object types, Arrays and Non-Nullability

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,23 @@
 export const resolvers = {
-    Query: {
-        greeting: () => 'Hello world',
-    }
-}
+  Query: {
+    jobs: () => {
+      return [
+        {
+          id: "test-id-1",
+          title: "Hello Emmanuel",
+          description: "You are a full blown developer",
+        },
+        {
+          id: "test-id-2",
+          title: "Hello Samuel",
+          description: "You are a full blown Doctor",
+        },
+        {
+          id: "test-id-3",
+          title: "Hello Comfort",
+          description: "You are a full blown Diplomat",
+        },
+      ];
+    },
+  },
+};
