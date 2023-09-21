447d0369469bdcb0b73c6afcfcbfa05d9a66117a
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 14:14:19 +0100

    clean code

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,27 +1,6 @@
 import { getJobs } from "./db/jobs.js";
 export const resolvers = {
   Query: {
-    jobs: async () => {
-      //   const jobs = await getJobs();
-      //     console.log(jobs)
-      return getJobs();
-      //   return [
-      //     {
-      //       id: "test-id-1",
-      //       title: "Hello Emmanuel",
-      //       description: "You are a full blown developer",
-      //     },
-      //     {
-      //       id: "test-id-2",
-      //       title: "Hello Samuel",
-      //       description: "You are a full blown Doctor",
-      //     },
-      //     {
-      //       id: "test-id-3",
-      //       title: "Hello Comfort",
-      //       description: "You are a full blown Diplomat",
-      //     },
-      //   ];
-    },
+    jobs: async () => getJobs(),
   },
 };
