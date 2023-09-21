f3eaa9f87abe6163a00b848a2d8079f6d092f0d1
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 15:07:46 +0100

    Object association, connect databases

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,4 @@
+import { getCompany } from "./db/companies.js";
 import { getJobs } from "./db/jobs.js";
 export const resolvers = {
   Query: {
@@ -5,14 +6,21 @@ export const resolvers = {
   },
 
   Job: {
+    company: (job) => {
+      return getCompany(job.companyId);
+      // return {
+      //     id: 'test-id',
+      //     name: 'my-company'
+      // }
+    },
     date: (job) => {
       //   console.log("resolving date for job", job);
       //   return "2022-12-31";
-      return toIsoDate(job.createdAt)
+      return toIsoDate(job.createdAt);
     },
   },
 };
 
-function toIsoDate(value){
-    return value.slice(0, "yyyy-mm-dd".length)
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
 }
