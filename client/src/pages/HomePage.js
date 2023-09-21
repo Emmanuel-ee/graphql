8fd258b605238b8ffb24ee29d1a9c9b8c4ef1810
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-21 15:44:56 +0100

    fetch the results, Display result using Component state in react

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,19 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
-import { getJobs } from '../lib/qraphql/queries';
+import { useState, useEffect } from "react";
+import JobList from "../components/JobList";
+// import { jobs } from '../lib/fake-data';
+import { getJobs } from "../lib/qraphql/queries";
 
-getJobs().then((jobs) => console.log('jobs:', jobs))
+// getJobs().then((jobs) => console.log('jobs:', jobs))
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs));
+  }, []);
+  console.log("[Homepage] jobs:", jobs);
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
