1310ba850a6c2ba50e33a16e06cafa7bccfe920c
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-22 15:23:42 +0100

    Handle error on the client side, Request states, Company page

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,28 +1,50 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { getCompany } from "../lib/qraphql/queries";
-import JobList from '../components/JobList';
+import JobList from "../components/JobList";
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
-  const [company, setCompany] = useState();
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({
+          company,
+          loading: false,
+          error: false,
+        });
+      } catch {
+        console.log("error:", JSON.stringify(error, null, 2));
+        setState({
+          company: null,
+          loading: false,
+          error: true,
+        });
+      }
+    })();
   }, [companyId]);
 
-  if (!company) {
+  console.log("[CompanyPage] state:", state);
+  const { company, loading, error } = state;
+  if (loading) {
     return <div>Loading...</div>;
   }
+  if (error) {
+    return <div className="has-text-danger">Data Unavailable</div>;
+  }
 
-  
   return (
     <div>
       <h1 className="title">{company.name}</h1>
       <div className="box">{company.description}</div>
-      <h2 className="title is-5">
-        Jobs at {company.name}
-      </h2>
+      <h2 className="title is-5">Jobs at {company.name}</h2>
       <JobList jobs={company.jobs} />
     </div>
   );
