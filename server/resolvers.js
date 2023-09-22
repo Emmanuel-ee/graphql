4c40b49f30c8ba9812be946d3c7df107bad1f1cd
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-22 17:43:02 +0100

    update Job graphQL

--- server/resolvers.js
+++ server/resolvers.js
@@ -5,6 +5,7 @@ import {
   getJob,
   getJobs,
   getJobsByCompany,
+  updateJob,
 } from "./db/jobs.js";
 import { GraphQLError } from "graphql";
 export const resolvers = {
@@ -32,8 +33,16 @@ export const resolvers = {
       const companyId = "FjcJCHJALA4i"; // TODO set based on user
       return createJob({ companyId, title, description });
     },
-    
-    deleteJob: (_root, { input: { id } }) => deleteJob(id),
+
+    deleteJob: (_root, {id}) => deleteJob(id),
+
+    updateJob: async (_root, { input: { id, title, description } }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("No job found with id " + id);
+      }
+      return updateJob({id, title, description});
+    },
   },
 
   Company: {
