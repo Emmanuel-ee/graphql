460b5b89e24031c24eb78f70899a76a04058518c
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-09-22 14:53:06 +0100

    set custom error and message

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,24 @@
 import { getCompany } from "./db/companies.js";
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { GraphQLError } from "graphql";
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No Company found with id " + id);
+      }
+      return company;
+    },
     jobs: async () => getJobs(),
-    job: (_root, { id }) => getJob(id),
+
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("No job found with id " + id);
+      }
+      return job;
+    },
   },
 
   Company: {
@@ -16,6 +30,12 @@ export const resolvers = {
     date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function notFoundError(message) {
+  throw new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
+}
 
 function toIsoDate(value) {
   return value.slice(0, "yyyy-mm-dd".length);
